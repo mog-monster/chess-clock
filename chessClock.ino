e325d82f777122f63ec35d04f01a70d16bfca87d
mog-monster <morten.ounsted@gmail.com>
2023-11-12 11:42:31 +0100

    realising the rationale behind all variables being unsigned did not apply, removed it and changed conditions to be milliSeconds <= 0 and running is triggered in deciSeconds instead.

--- chessClock.ino
+++ chessClock.ino
@@ -10,30 +10,30 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 static bool running;
-unsigned long removalAmount = 6000000;
-unsigned long milliMinus = millis();
-unsigned long milliSeconds = removalAmount - milliMinus;
-unsigned long centiSeconds = milliSeconds / 10;
-unsigned long printedCentiSeconds = centiSeconds % 10;
-unsigned long deciSeconds = milliSeconds/100;
-unsigned long printedDeciSeconds = deciSeconds % 10;
-unsigned long seconds = milliSeconds/1000;
-if(seconds==1){
+long removalAmount = 10000;
+long milliMinus = millis();
+long milliSeconds = removalAmount - milliMinus;
+long centiSeconds = milliSeconds / 10;
+long printedCentiSeconds = centiSeconds % 10;
+long deciSeconds = milliSeconds/100;
+if(deciSeconds==1){
   running = 1;
 }
-unsigned long printedSeconds = seconds % 10;
-unsigned long decaSeconds = milliSeconds/10000;
-unsigned long printedDecaSeconds = decaSeconds % 6;
-unsigned long minutes = milliSeconds/60000;
-unsigned long printedMinutes = minutes % 10;
-unsigned long decaMinutes = milliSeconds/600000;
-unsigned long printedDecaMinutes = decaMinutes % 10;
-unsigned long hectoMinutes = milliSeconds/6000000;
+long printedDeciSeconds = deciSeconds % 10;
+long seconds = milliSeconds/1000;
+long printedSeconds = seconds % 10;
+long decaSeconds = milliSeconds/10000;
+long printedDecaSeconds = decaSeconds % 6;
+long minutes = milliSeconds/60000;
+long printedMinutes = minutes % 10;
+long decaMinutes = milliSeconds/600000;
+long printedDecaMinutes = decaMinutes % 10;
+long hectoMinutes = milliSeconds/6000000;
 
 if(running){
-  if((milliSeconds == 0) || (hectoMinutes > 2)){
+  if(milliSeconds <= 0){
     Serial.println("0 : 0 : 0 :: 0 : 0 :: 0 : 0");
-    while((milliSeconds == 0) || (hectoMinutes > 2)){
+    while(true){
       delay(1);
     }
   }
