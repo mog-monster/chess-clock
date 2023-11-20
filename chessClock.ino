115d450f41b7955543795818d667c98b35bbc29a
mog-monster <morten.ounsted@gmail.com>
2023-11-20 19:00:33 +0100

    proper indentation

--- chessClock.ino
+++ chessClock.ino
@@ -22,40 +22,39 @@ void setup ()
 
 void loop() {
 
-static bool running;
-long removalAmount = 10000;
-long milliMinus = millis();
-long milliSeconds = removalAmount - milliMinus;
-long centiSeconds = milliSeconds / 10;
-long printedCentiSeconds = centiSeconds % 10;
-long deciSeconds = milliSeconds/100;
-if(deciSeconds==1){
-  running = 1;
-}
-long printedDeciSeconds = deciSeconds % 10;
-long seconds = milliSeconds/1000;
-long printedSeconds = seconds % 10;
-long decaSeconds = milliSeconds/10000;
-long printedDecaSeconds = decaSeconds % 6;
-long minutes = milliSeconds/60000;
-long printedMinutes = minutes % 10;
-long decaMinutes = milliSeconds/600000;
-long printedDecaMinutes = decaMinutes % 10;
-long hectoMinutes = milliSeconds/6000000;
-long allSegments [7] = {printedCentiSeconds, printedDeciSeconds, printedSeconds, printedDecaSeconds, printedMinutes, printedDecaMinutes, hectoMinutes};	
+  static bool running;
+  long removalAmount = 10000;
+  long milliMinus = millis();
+  long milliSeconds = removalAmount - milliMinus;
+  long centiSeconds = milliSeconds / 10;
+  long printedCentiSeconds = centiSeconds % 10;
+  long deciSeconds = milliSeconds/100;
+  if(deciSeconds==1){
+    running = 1;
+  }
+  long printedDeciSeconds = deciSeconds % 10;
+  long seconds = milliSeconds/1000;
+  long printedSeconds = seconds % 10;
+  long decaSeconds = milliSeconds/10000;
+  long printedDecaSeconds = decaSeconds % 6;
+  long minutes = milliSeconds/60000;
+  long printedMinutes = minutes % 10;
+  long decaMinutes = milliSeconds/600000;
+  long printedDecaMinutes = decaMinutes % 10;
+  long hectoMinutes = milliSeconds/6000000;
+  long allSegments [7] = {printedCentiSeconds, printedDeciSeconds, printedSeconds, printedDecaSeconds, printedMinutes, printedDecaMinutes, hectoMinutes};	
 
-if(running){
-  if(milliSeconds <= 0){
-    while(true){
-      delay(1);
+  if(running){
+    if(milliSeconds <= 0){
+      while(true){
+        delay(1);
+      }
     }
   }
-}
-digitalWrite(latchPin, LOW);
-for (int workingSegment = 0; workingSegment < 7; workingSegment++){
-	
-    shiftOut(dataPin, clockPin, MSBFIRST, allBytes[allSegments[workingSegment]]);
+  digitalWrite(latchPin, LOW);
+  for (int workingSegment = 0; workingSegment < 7; workingSegment++){
+      shiftOut(dataPin, clockPin, MSBFIRST, allBytes[allSegments[workingSegment]]);
 
-}
-digitalWrite(latchPin, HIGH);
+  }
+  digitalWrite(latchPin, HIGH);
 }
