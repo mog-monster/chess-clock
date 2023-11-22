039e33c721e48ea6686ce824f15fe6a7c5105f04
mog-monster <morten.ounsted@gmail.com>
2023-11-22 20:25:27 +0100

    Currently the program succesfully switches between the two states, however it starts from 0 and the timer doesn't actually pause it, just displays 0

--- chessClock.ino
+++ chessClock.ino
@@ -1,59 +1,89 @@
-// here will be my chess clock stuff
+long totalMilliSeconds = 10000;
+int mainButtonPin = A5;
 
 void setup() {
-  // put your setup code here, to run once:
-
-Serial.begin(9600);
+  
+  pinMode(mainButtonPin, INPUT_PULLUP);
+		Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-static bool running;
-long removalAmount = 10000;
-long milliMinus = millis();
-long milliSeconds = removalAmount - milliMinus;
-long centiSeconds = milliSeconds / 10;
-long printedCentiSeconds = centiSeconds % 10;
-long deciSeconds = milliSeconds/100;
-if(deciSeconds==1){
-  running = 1;
+  
+    static bool mainButtonChanged;
+    static bool timersRunning;
+    static bool timersFinished;
+    bool mainButtonPressed = digitalRead(mainButtonPin);
+    mainButtonPressed = !mainButtonPressed;
+    if((mainButtonPressed) && (!mainButtonChanged)){
+        mainButtonChanged = 1;
+        timersRunning = !timersRunning;
 }
-long printedDeciSeconds = deciSeconds % 10;
-long seconds = milliSeconds/1000;
-long printedSeconds = seconds % 10;
-long decaSeconds = milliSeconds/10000;
-long printedDecaSeconds = decaSeconds % 6;
-long minutes = milliSeconds/60000;
-long printedMinutes = minutes % 10;
-long decaMinutes = milliSeconds/600000;
-long printedDecaMinutes = decaMinutes % 10;
-long hectoMinutes = milliSeconds/6000000;
+    mainButtonChanged = mainButtonPressed;  
+  if(timersRunning){
+    totalMilliSeconds = white(totalMilliSeconds);
+  }
+  else{
+    totalMilliSeconds =  pausedTimers(totalMilliSeconds);
 
-if(running){
-  if(milliSeconds <= 0){
-    Serial.println("0 : 0 : 0 :: 0 : 0 :: 0 : 0");
-    while(true){
-      delay(1);
-    }
   }
 }
 
-Serial.print(hectoMinutes);
-Serial.print(" : ");
-Serial.print(printedDecaMinutes);
-Serial.print(" : ");
-Serial.print(printedMinutes);
-Serial.print(" :: ");
-Serial.print(printedDecaSeconds);
-Serial.print(" : ");
-Serial.print(printedSeconds);
-Serial.print(" :: ");
-Serial.print(printedDeciSeconds);
-Serial.print(" : ");
-Serial.println(printedCentiSeconds);
-
-
+long pausedTimers(long milliSeconds){
+  long centiSeconds = milliSeconds / 10;
+  long printedCentiSeconds = centiSeconds % 10;
+  long deciSeconds = milliSeconds/100;
+  long printedDeciSeconds = deciSeconds % 10;
+  long seconds = milliSeconds/1000;
+  long printedSeconds = seconds % 10;
+  long decaSeconds = milliSeconds/10000;
+  long printedDecaSeconds = decaSeconds % 6;
+  long minutes = milliSeconds/60000;
+  long printedMinutes = minutes % 10;
+  long decaMinutes = milliSeconds/600000;
+  long printedDecaMinutes = decaMinutes % 10;
+  long hectoMinutes = milliSeconds/6000000;
+  Serial.print(hectoMinutes);
+  Serial.print(" : ");
+  Serial.print(printedDecaMinutes);
+  Serial.print(" : ");
+  Serial.print(printedMinutes);
+  Serial.print(" :: ");
+  Serial.print(printedDecaSeconds);
+  Serial.print(" : ");
+  Serial.print(printedSeconds);
+  Serial.print(" :: ");
+  Serial.print(printedDeciSeconds);
+  Serial.print(" : ");
+  Serial.println(printedCentiSeconds);
 }
 
-
+long white(long milliSeconds){
+  long milliMinus = millis();
+  milliSeconds = milliSeconds - milliMinus;
+  long centiSeconds = milliSeconds / 10;
+  long printedCentiSeconds = centiSeconds % 10;
+  long deciSeconds = milliSeconds/100;
+  long printedDeciSeconds = deciSeconds % 10;
+  long seconds = milliSeconds/1000;
+  long printedSeconds = seconds % 10;
+  long decaSeconds = milliSeconds/10000;
+  long printedDecaSeconds = decaSeconds % 6;
+  long minutes = milliSeconds/60000;
+  long printedMinutes = minutes % 10;
+  long decaMinutes = milliSeconds/600000;
+  long printedDecaMinutes = decaMinutes % 10;
+  long hectoMinutes = milliSeconds/6000000;
+  Serial.print(hectoMinutes);
+  Serial.print(" : ");
+  Serial.print(printedDecaMinutes);
+  Serial.print(" : ");
+  Serial.print(printedMinutes);
+  Serial.print(" :: ");
+  Serial.print(printedDecaSeconds);
+  Serial.print(" : ");
+  Serial.print(printedSeconds);
+  Serial.print(" :: ");
+  Serial.print(printedDeciSeconds);
+  Serial.print(" : ");
+  Serial.println(printedCentiSeconds);
+}
